fb269b5a26c36de70d1d525ed7ff929c97975c37
joaquinmora14 <joacomora13@gmail.com>
2025-05-16 10:36:50 -0300

    clonar cubos terminado

---
+++ Assets/ClonaCubos.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClonaCubos : MonoBehaviour
+{
+
+    public GameObject cuboPrefab;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Instantiate(cuboPrefab);
+
+
+    }
+
+    public void ClonarCubos()
+    {
+        Instantiate(cuboPrefab);
+    }
+}
